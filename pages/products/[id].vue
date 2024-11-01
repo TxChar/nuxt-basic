ae07ff70ed11f6ce5614eb93c72afebf52fa8cc6
TxChar <pontogreen1@gmail.com>
2024-11-01 20:35:25 +0700

    feat: 🎸 Add product's dynamic route

---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div>
+            <h1>
+                Products {{id}}
+            </h1>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const {id} = useRoute().params
+</script>
+
+<style scoped></style>
