5d5b70acd6c580500a7fc8bdfcdf80d9f906a07b
TxChar <pontogreen1@gmail.com>
2024-11-01 21:04:29 +0700

    feat: 🎸 Fetching data from dummy json and display

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,13 +1,49 @@
 <template>
-    <div>
-        <h1>Products</h1>
+    <h1 style="text-align: center;">All Products</h1>
+    <div class="container">
+        <div v-for="product in data.products">
+            <NuxtLink :to="`/products/${product.id}`">
+                <h2 class="title">
+                    {{ product.title }}
+                </h2>
+                <img :src=product.thumbnail>
+            </NuxtLink>
+        </div>
     </div>
 </template>
 
 <script setup>
-
+    const { data } = await useFetch('https://dummyjson.com/products?limit=12')
+    console.log(data)
 </script>
 
 <style scoped>
+.container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-bottom: 5px;
+}
 
+.container div{
+    width: 300px;
+    height: 300px;
+    margin: 30px 15px;
+    background: #333;
+}
+
+.container div:hover{
+    background-color: orange;
+}
+
+.title {
+    color: white;
+    text-align: center;
+    font-size: 1.2em;
+}
+
+/* img {
+    height: 200px;
+    width: 200px;
+} */
 </style>
